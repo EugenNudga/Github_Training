d6c0ae3dc5b0d2dc1e3fb5734509a094c60cd513
EugenNudga <evgeniybond@gmail.com>
2019-03-10 12:48:43 +0100

    rename

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,16 +4,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        int i0= 0;
-        System.out.println(String.format("%d", i0));
+        int first= 0;
+        System.out.println(String.format("%d", first));
         int i1 = 1;
         System.out.println(String.format("%d", i1));
 
         while(true)
         {
-            int i2 = i1 + i0;
+            int i2 = i1 + first;
             System.out.println(String.format("%d", i2));
-            i0 = i1;
+            first = i1;
             i1 = i2;
         }
 
